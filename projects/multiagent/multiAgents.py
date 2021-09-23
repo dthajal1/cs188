4f72406d84f07984be17eba5ae491df655b61393
Diraj Thajali <thajalidiraj@gmail.com>
2021-09-22 18:34:34 -0700

    minimax is workinggit add .git add .

--- projects/multiagent/multiAgents.py
+++ projects/multiagent/multiAgents.py
@@ -15,6 +15,7 @@
 from util import manhattanDistance
 from game import Directions
 import random, util
+from math import inf
 
 from game import Agent
 
@@ -95,7 +96,7 @@ class ReflexAgent(Agent):
         else:
             # closer ghost => lower score 
             return successorGameState.getScore() + min(mhDistToGhosts)
-            
+
         # return successorGameState.getScore() # default
 
 def scoreEvaluationFunction(currentGameState):
@@ -157,7 +158,50 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        def minimaxAlgorithm(gameState, agentIndex, currDepth):
+            if currDepth == self.depth or gameState.isWin() or gameState.isLose():
+                # Terminal state => return state utility
+                return self.evaluationFunction(gameState)
+
+            if agentIndex == 0:
+                # pacman's turn -- maximizer
+                bestVal = -inf
+                actions = gameState.getLegalActions(agentIndex)
+                for action in actions:  
+                    successorGameState = gameState.generateSuccessor(agentIndex, action)
+                    possibleBestVal = minimaxAlgorithm(successorGameState, agentIndex + 1, currDepth)
+                    bestVal = max(bestVal, possibleBestVal)
+                return bestVal
+            else:
+                # ghosts' turn -- minimizer
+                bestVal = inf
+                actions = gameState.getLegalActions(agentIndex)
+                for action in actions:  
+                    successorGameState = gameState.generateSuccessor(agentIndex, action)
+                    if agentIndex + 1 == gameState.getNumAgents():
+                        # this is the last ghost so pacman will go next
+                        # and since we are done with iterating through pacman and all ghosts we can now increment the depth by 1
+                        possibleBestVal = minimaxAlgorithm(successorGameState, 0, currDepth + 1)
+                    else:
+                        # there are other ghosts left waiting for their turn
+                        possibleBestVal = minimaxAlgorithm(successorGameState, agentIndex + 1, currDepth)
+                    bestVal = min(bestVal, possibleBestVal)
+                return bestVal
+
+        # Collect legal moves and successor states
+        legalMoves = gameState.getLegalActions(self.index)
+
+        bestVal = -inf
+        actionToTake = Directions.STOP
+        for action in legalMoves:
+            successorGameState = gameState.generateSuccessor(self.index, action)
+            possibleBestVal = minimaxAlgorithm(successorGameState, self.index + 1, 0)
+            if (possibleBestVal > bestVal):
+                bestVal = possibleBestVal
+                actionToTake = action
+
+        return actionToTake   
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
